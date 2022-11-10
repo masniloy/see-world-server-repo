1a3a0a11d364f152b24a4b05c0885c32aad83171
masniloy <masaimunniloy88@gmail.com>
2022-11-10 14:49:29 +0600

    db done

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const uri = "mongodb+srv://SeeWorldUser:F0zito2DweD9xaCA@cluster0.g924zd8.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g924zd8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-
+        const services = client.db('SeeWorld').collection('Services')
+        app.get('/Services', async (req, res) => {
+            const query = {}
+            const cursor = services.find(query);
+            const Services = await cursor.toArray();
+            res.send(Services);
+        })
     }
     finally {
 
