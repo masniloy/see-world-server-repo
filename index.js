bf0ad3147471bfb34a4ca5c6510bb7936ef21cd2
masniloy <masaimunniloy88@gmail.com>
2022-11-12 14:50:22 +0600

    another api

--- index.js
+++ index.js
@@ -10,16 +10,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g924zd8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client2 = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v2 });
 
 async function run() {
     try {
         const services = client.db('SeeWorld').collection('Services')
+        const service = client2.db('SeeWorld').collection('Service')
         app.get('/Services', async (req, res) => {
             const query = {}
             const cursor = services.find(query);
             const Services = await cursor.toArray();
             res.send(Services);
         })
+
+        app.get("/Service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const Service = await service.findOne(query);
+            res.send(Service);
+        });
+
     }
     finally {
 
@@ -27,6 +37,26 @@ async function run() {
 
 }
 run().catch(err => console.log(err));
+
+
+
+// async function run() {
+//     try {
+//         const service = client.db('SeeWorld').collection('Service')
+//         app.get('/Service/:_id', async (req, res) => {
+//             const _id = req.params._id
+//             const query = { _id: Object(_id) };
+//             const cursor = service.find(query);
+//             const Service = await cursor.toArray(query);
+//             res.send(Service);
+//         })
+//     }
+//     finally {
+
+//     }
+
+// }
+
 
 
 app.get('/', (req, res) => {
